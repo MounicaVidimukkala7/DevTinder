5cc6fa67dff737b92460e25a0c37d1be4b34a90d
Mounica <mounica.vidimukkala@gmail.com>
2025-05-10 15:24:22 +0530

    Removed src

--- Src/config/database.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose")
-const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://mounicavidimukkala:Scaler%40199321@devtinder.jr8m3wx.mongodb.net/Tinder");
-}
-
-module.exports = connectDB
